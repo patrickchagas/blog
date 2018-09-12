8f0db942cab3646bc028d25d8e26a7b732731bc3
Patrick Chagas <patrickchagas21@gmail.com>
2018-09-12 10:49:23 -0300

    Criei a Classe Page

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new Slim\Slim();
+use \Slim\Slim;
+use \Pcode\Page;
+
+$app = new Slim();
+
+$app->config('debug', true);
 
 $app->get('/', function() {
 
-	$sql = new Pcode\DB\Sql();
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
